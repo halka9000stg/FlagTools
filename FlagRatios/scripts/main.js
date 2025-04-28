824d61c3514b4274516d88689934560640b9137e
佐藤陽花 <as.he.that.doth.serve@gmail.com>
2025-04-29 02:35:06 +0900

    main.js の更新

--- FlagRatios/scripts/main.js
+++ FlagRatios/scripts/main.js
@@ -1,12 +1,12 @@
 Math.isInteger = (n) => Math.trunc(n) == n;
 Math.len = (n) => Math.lenInt(n) + (Math.isInteger(n) ? 0 : Math.lenFrac(n));
 Math.lenInt =  (n) => (Math.abs(Math.floor(Math.log10(Math.trunc(n))) + 1) == Infinity) ? 1 : Math.floor(Math.log10(n)) + 1;
-Math.lenFrac =  function(number) {
+Math.lenFrac = (n) => {
   if(Math.isInteger(n)) return 0;
-	let numbers = String(number).split('.');
-	return numbers[1] ? numbers[1].length : 0;
+	let ns = String(n).split('.');
+	return ns[1] ? ns[1].length : 0;
 };
-Math.gcd = (x, y) => x % y ? gcd(y, x % y) : y;
+Math.gcd = (x, y) => x % y ? Math.gcd(y, x % y) : y;
 Math.rti = (x, y) => {
   let mxl = Math.max(Math.lenFrac(x), Math.lenFrac(y));
   let mxp = Math.pow(10, mxl);
@@ -31,16 +31,16 @@ function buildSvg(x, y, hex){
 
   return`<?xml version="1.0" encoding="UTF-8"?>
 <!doctype svg>
-<svg xmlns="http://www.w3.org/2000/svg" width="{wfo}" height="{hfo}" viewBox="0 0 {wf} {hf}">
-  <path d="M 0 0  h {wf} v {hf} h -{wf} z" fill="{hex}" />
+<svg xmlns="http://www.w3.org/2000/svg" width="${wfo}" height="${hfo}" viewBox="0 0 ${wf} ${hf}">
+  <path d="M 0 0  h ${wf} v ${hf} h -${wf} z" fill="${hex}" />
 </svg>`;
 }
 function string2url(str){
   return "data:text/plain;charset=UTF-8," + encodeURIComponent(str);
 }
 function getInputs(){
-  let h =document.querySelector("input#height").value;
-  let w = document.querySelector("input#width").value;
+  let h = parseFloat(document.querySelector("input#height").value);
+  let w = parseFloat(document.querySelector("input#width").value);
   let c = document.querySelector("input#colour").value;
   return {height: h, width: w, colour: c};
 }
@@ -53,6 +53,6 @@ document
       event.target.setAttribute("href",
         string2url(buildSvg(conf.height, conf.width, conf.colour)));
       event.target.setAttribute("download",
-      `flag_{conf.colour.substring(1)}_{conf.height}x{conf.width}.svg`
+      `flag_${conf.colour.substring(1)}_${conf.height}x${conf.width}.svg`
     },
   );
